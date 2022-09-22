634bef1e39137e3a3590b2fa19fc0afb23f40d50
Ramunas P <ramunas@pythonkursas.smshostingas.lt>
2022-09-22 23:01:11 +0300

    6 paskaita, 2 dalis, puslapiavimas per class views ir function (pataisyta author.html ir jame pridėtas linkas į knygas)

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -8,18 +8,14 @@ from .models import Book, Author, BookInstance
 def index(request):
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-
     num_instances_available = BookInstance.objects.filter(status__exact="g").count()
-
     num_authors = Author.objects.count()
-
     kontext = {
         "num_books": num_books,
         "num_instances": num_instances,
         "num_instances_available": num_instances_available,
         "num_authors": num_authors
     }
-
     return render(request, "index.html", context=kontext)
 
 
